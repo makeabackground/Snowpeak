e920ed93748eb773e8b99e9b8f0391d22e2ac820
makeabackground <jennifer.dangelo@tcu.edu>
2026-03-17 11:23:44 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+  
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
